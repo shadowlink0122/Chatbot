df838df08cc2db86ebd4563000ff39e71a920a3d
Miyajima Kenta <shadowlink0122@gmail.com>
2018-09-03 00:33:08 +0900

    test

--- main.py
+++ main.py
@@ -3,8 +3,27 @@ from linebot.models import (TextSendMessage, )
 import botengine
 
 def create_message(input):
+    love = ['好き', 'すき', 'スキ']
+    unlove = ['嫌い', 'きらい', 'キライ']
+    jankenstart = ['じゃんけん', 'ジャンケン']
+    message = ''
 
-  # message = botengine.make_reply(input)
-  message = "" + input
+    for word in love:
+        if input.find(word) >= 0:
+            message = 'だよね。寺子屋大好き❤️'
 
-  return TextSendMessage(text=message)
+    for word in unlove:
+        if input.find(word) >= 0:
+            message = 'は？ふざけんなよ！！'
+
+    for word in jankenstart:
+        if input.find(word) >= 0 or janken_flag ==1:
+            if janken_flag != 1:
+                message = 'グー・チョキ・パーのどれかを入力してね'
+            else:
+                janken_message(input)
+
+    if len(message) == 0:
+        message = input + '、と言いましたね？'
+
+    return TextSendMessage(text=message)
