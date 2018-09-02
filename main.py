3938765bc6def1483735fd42665e66451a8bfaa6
Miyajima Kenta <shadowlink0122@gmail.com>
2018-09-02 13:38:35 +0900

    Change send message

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from linebot.models import (TextSendMessage, )
 import botengine as bt
 
 def create_message(comment):
-	message = ""
-  message = tb.make_reply(comment)
+	message = "test message"
+  # message = tb.make_reply(comment)
 
   if len(message) < 1:
   	message = "Error"
