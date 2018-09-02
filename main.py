c11535a42e42d8961b3bdecc45e9758c15f98ce5
Miyajima Kenta <shadowlink0122@gmail.com>
2018-09-02 22:56:24 +0900

    change main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from linebot.models import (TextSendMessage, )
 import botengine
 
 def create_message(input):
-<<<<<<< HEAD
-  message = ''
 
   # for word in love:
   #     if input.find(word) >= 0:
@@ -14,13 +12,7 @@ def create_message(input):
   #     if input.find(word) >= 0:
   #         message = 'は？ふざけんなよ！！'
 
-  if len(message) == 0:
     # message = botengine.make_reply(input)
     message = botengine.make_reply(input)
 
   return TextSendMessage(text=message)
-=======
-    message = botengine.make_reply(input)
-
-    return TextSendMessage(text=message)
->>>>>>> 5a9efa352a49547fd9b8e966fa541ebc1215c55e
