d0f9936b5b094b40c63fbb391cc572efa7138a00
Miyajima Kenta <shadowlink0122@gmail.com>
2018-09-02 13:36:39 +0900

    Change send message

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import random
-
 from linebot.models import (TextSendMessage, )
 import botengine as bt
 
 def create_message(comment):
+	message = ""
+  message = tb.make_reply(comment)
 
-    message = tb.make_reply(comment)
+  if len(message) < 1:
+  	message = "Error"
 
-    return TextSendMessage(text=message)
+  return TextSendMessage(text=message)
