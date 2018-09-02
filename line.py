530047f59a88d1e472651344d820c710c7b441ae
Miyajima Kenta <shadowlink0122@gmail.com>
2018-09-03 02:19:19 +0900

    a bit change

--- line.py
+++ line.py
@@ -18,13 +18,13 @@ from argparse import ArgumentParser
 
 from flask import Flask, request, abort
 from linebot import (
-    LineBotApi, WebhookHandler
+	LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+	InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+	MessageEvent, TextMessage, TextSendMessage,
 )
 
 import main
@@ -35,40 +35,40 @@ app = Flask(__name__, static_folder='images')
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
+	print('Specify LINE_CHANNEL_SECRET as environment variable.')
+	sys.exit(1)
 if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
+	print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+	sys.exit(1)
 
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+	# get X-Line-Signature header value
+	signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+	# get request body as text
+	body = request.get_data(as_text=True)
+	app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+	# handle webhook body
+	try:
+		handler.handle(body, signature)
+	except InvalidSignatureError:
+		abort(400)
 
-    return 'OK'
+	return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        main.create_message(event.message.text)
-    )
+	line_bot_api.reply_message(
+		event.reply_token,
+		main.create_message(event.message.text)
+	)
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+	port = int(os.getenv("PORT", 5000))
+	app.run(host="0.0.0.0", port=port)
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from linebot.models import (TextSendMessage, )
 # import botengine
 
 def create_message(input):
-		message = ""
+	message = ""
 		
-    # message = botengine.make_reply(input)
+	# message = botengine.make_reply(input)
 
-    if len(message) == 0:
-        message = '今はやめて...'
+	if len(message) == 0:
+		message = '今はやめて...'
 
-    return TextSendMessage(text=message)
+	return TextSendMessage(text=message)
