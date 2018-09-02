2e1cae925c9b3f0cb0df2702705b6f7148916a9e
Miyajima Kenta <shadowlink0122@gmail.com>
2018-09-02 22:01:47 +0900

    use Json file

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from linebot.models import (TextSendMessage, )
 import botengine
 
 def create_message(input):
-    message = ''
+  message = ''
 
-    # for word in love:
-    #     if input.find(word) >= 0:
-    #         message = 'だよね。寺子屋大好き❤️'
+  # for word in love:
+  #     if input.find(word) >= 0:
+  #         message = 'だよね。寺子屋大好き❤️'
 
-    # for word in unlove:
-    #     if input.find(word) >= 0:
-    #         message = 'は？ふざけんなよ！！'
+  # for word in unlove:
+  #     if input.find(word) >= 0:
+  #         message = 'は？ふざけんなよ！！'
 
-    if len(message) == 0:
-        # message = botengine.make_reply(input)
-        message = botengine.make_reply(input)
+  if len(message) == 0:
+    # message = botengine.make_reply(input)
+    message = botengine.make_reply(input)
 
-    return TextSendMessage(text=message)
+  return TextSendMessage(text=message)
