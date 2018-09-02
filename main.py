82f03bc8db25704368cd8092dffdedf4188049cc
Miyajima Kenta <shadowlink0122@gmail.com>
2018-09-03 01:08:25 +0900

    a bit change

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import random
 from linebot.models import (TextSendMessage, )
-import botengine as bt
+# import botengine as bt
 
 def create_message(input):
-    love = ['好き', 'すき', 'スキ']
-    unlove = ['嫌い', 'きらい', 'キライ']
-    jankenstart = ['じゃんけん', 'ジャンケン']
     message = ''
 
     if len(message) == 0:
