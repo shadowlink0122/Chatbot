8237a009590302fb97ca59e63216c6538ed6f6d8
Miyajima Kenta <shadowlink0122@gmail.com>
2018-09-03 00:21:19 +0900

    test

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import botengine
 def create_message(input):
 
   message = botengine.make_reply(input)
+  message = "Test Message\nMaintenance now"
 
   if message == "":
   	message = "Error : なんでだろう🤔\n				整備中です🙇‍♀️"
