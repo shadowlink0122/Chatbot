8abe6dd90dbea38cc7100d55bb45c1d2b898d7a3
Miyajima Kenta <shadowlink0122@gmail.com>
2018-09-03 01:44:38 +0900

    a bit change

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import random
 from linebot.models import (TextSendMessage, )
 import botengine
 
 def create_message(input):
-    message = ''
+    message = botengine.make_reply(input)
 
     if len(message) == 0:
         message = '今はやめて...'
