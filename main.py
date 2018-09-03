6994f35fd5b69fa643eacc8639fee9b512ac84e5
Miyajima Kenta <shadowlink0122@gmail.com>
2018-09-03 14:43:34 +0900

    a bit change

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from linebot.models import (TextSendMessage, )
 # import botengine
 
 def create_message(input):
-	message = "ありがとう😭"
+	message = ""
 		
 	# message = botengine.make_reply(input)
 
 	if len(message) == 0:
-		message = 'メンテナンス中\n改装が整うまでお待ちください'
+		message = 'ただ今メンテナンス中\n改装が整うまでお待ちください'
 
 	return TextSendMessage(text=message)
